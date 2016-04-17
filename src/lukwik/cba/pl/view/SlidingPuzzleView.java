736ab6845bda473858962baea18c72a1f9b6b611
Łukasz Wiktorek <lukasz-wiktorek@wp.pl>
2016-04-17 19:08:41 +0200

    view

--- src/lukwik/cba/pl/view/SlidingPuzzleView.java
+++ src/lukwik/cba/pl/view/SlidingPuzzleView.java
@@ -1,5 +1,10 @@
 package lukwik.cba.pl.view;
 
+import lukwik.cba.pl.controller.SlidingPuzzleController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
@@ -9,10 +14,47 @@ import java.util.Observer;
  */
 public class SlidingPuzzleView implements Observer
 {
-    List modelState;
+    List pieces;
+    SlidingPuzzleController controller;
+    JFrame frameGame;
+    JButton buttonNewGame;
+    JButton buttonStart;
+    PiecesPanel panelPieces;
+
+    public  SlidingPuzzleView( SlidingPuzzleController c )
+    {
+        controller = c;
+    }
+
+    public void initialize()
+    {
+        pieces = new ArrayList();
+        frameGame = new JFrame("Sliding puzzle game");
+        buttonNewGame = new JButton("New Game");
+        buttonStart = new JButton("Start");
+        panelPieces = new PiecesPanel(pieces);
+
+    }
     @Override
     public void update(Observable o, Object arg)
     {
-        modelState = (List) arg;
+        List list = (List) arg;
+        pieces.clear();
+        pieces.addAll(list);
+    }
+}
+
+class PiecesPanel extends JPanel
+{
+    List pieces;
+
+    PiecesPanel(List p)
+    {
+        pieces = p;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
     }
 }
